4d99295caee75c5cfd5618b4d0f3e3b2733934ad
Sergei <sergei.issaev@vooban.com>
2022-06-02 16:45:09 -0400

    karte.py

---
+++ karte.py
@@ -0,0 +1,20 @@
+class Karte:
+    def __init__(self):
+        self.cards = input()
+
+
+    def print_missing(self):
+        history = list()
+        suits = dict(P=0, H=0, K=0, T=0)
+        for i in range(0, len(self.cards), 3):
+            card = self.cards[i:i+3]
+            if card in history:
+                return "GRESKA"
+            history.append(card)
+            suits[card[0]] += 1
+        return " ".join(str(a) for a in [13-suits[x] for x in ["P", "K", "H", "T"]])
+
+
+if __name__ == '__main__':
+    karte = Karte()
+    print(karte.print_missing())
